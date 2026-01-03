1d46b6f35392cd803479af88c9bce09d247edc7f
nhietthanh <66879602+nhietthanh@users.noreply.github.com>
2026-01-03 09:58:52 +0700

    init project

---
+++ src/app.js
@@ -0,0 +1,29 @@
+const compression = require('compression')
+const express = require('express')
+const { default: helmet } = require('helmet')
+const morgan = require('morgan')
+const app = express()
+
+// init middleware
+app.use(morgan('dev')) 
+app.use(helmet())
+app.use(compression())
+// init db
+// init routes
+
+
+
+app.get('/',(req,res, next)=>{
+
+    const strCompress = 'Hello Thanh'
+    return res.status(200).json({
+        message:'Wellcome anyone',
+        metadata:strCompress.repeat(10000)
+    })
+})
+
+
+
+// hanling error
+
+module.exports = app
